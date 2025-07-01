e64b3b41d52aa2b30128724ba0003164d43764e0
bduong <binhduong31725@gmail.com>
2025-07-01 20:23:31 +0700

    Lab 3 intent 4

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -30,13 +33,36 @@ public class DetailActivity extends AppCompatActivity {
             editor.apply();
         }
 
-        Button orderButton = findViewById(R.id.orderButton);
-        orderButton.setOnClickListener(v -> {
-            Toast.makeText(this, "Bạn đã gọi món: " + food.getName(), Toast.LENGTH_SHORT).show();
-            SharedPreferences prefs = getSharedPreferences("OrderedFood", MODE_PRIVATE);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString("orderedFoodName", food.getName());
-            editor.apply();
+        Button btnCall = findViewById(R.id.btnCall);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:0123456789"));
+                startActivity(callIntent);
+            }
+        });
+
+        Button btnMap = findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String geoUri = "geo:0,0?q=Com+tấm+Kieu+Giang";
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+        Button btnWeb = findViewById(R.id.btnWeb);
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://comtamkieugiang.vn/";
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(webIntent);
+            }
         });
     }
 }
